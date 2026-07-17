a6b78e6de5760b9272bc3ba087208630b9c568d7
Dhruv Sharma <sharma.dhruv.india@gmail.com>
2026-07-17 10:43:25 +0530

    second day java

---
+++ java02/challenge.java
@@ -0,0 +1,51 @@
+package java02;
+import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class challenge {
+    public static void main(String[] args) {
+        //challenge 1
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("enter the no");
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+        //challenge 2
+        int[] arr={2,1,4,5,6};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+        int avg=sum/arr.length;
+        System.out.println(avg);
+        Arrays.sort(arr);
+        System.out.println("max element is:"+arr[0]);
+        System.out.println("mim element is:"+arr[arr.length-1]);
+
+        //challenge 3
+        int count = 0;
+
+        for (int i = 2; i <= 50; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+                count++;
+            }
+        }
+
+        System.out.println("\nTotal prime numbers = " + count);
+    }
+}
---
+++ java02/second.java
@@ -0,0 +1,29 @@
+package java02;
+import java.util.Scanner;
+
+public class second {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Quotient: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+
+        int x = 10, y = 20;
+        System.out.println(x > y); // false
+        System.out.println(x < y); // true
+        System.out.println(x == y); // false
+        System.out.println(x != y); // true
+        System.out.println(x >= 10); // true
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num=sc.nextInt();
+        System.out.println("You entered: " + num);
+
+
+    }
+}
